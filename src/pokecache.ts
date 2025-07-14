d461ec458175bb7d06296105e7252309c0f8897e
Dmitri Dmitriev <d.dzmitryeu@gmail.com>
2025-07-14 18:28:08 +0300

    cache improvement

---
+++ src/pokecache.ts
@@ -0,0 +1,51 @@
+export type CacheEntry<T> = {
+  createdAt: number,
+  val: T,
+}
+
+export class Cache {
+  #cache = new Map<string, CacheEntry<any>>();
+
+  #reapIntervalId: NodeJS.Timeout | undefined = undefined;
+
+  #interval: number;
+
+  constructor(interval: number) {
+    this.#interval = interval
+    this.#startReapLoop()
+  }
+
+  add<T>(key: string, val: T) {
+    const value = {
+      createdAt: Date.now(),
+      val: val
+    }
+    this.#cache.set(key, value)
+  }
+
+  get<T>(key: string) {
+    const result = this.#cache.get(key)
+    return result ? result : undefined
+  }
+
+  #reap() {
+    for (const [key, val] of this.#cache) {
+      if (Date.now() - this.#interval > val.createdAt) {
+        this.#cache.delete(key)
+      }
+    }
+  }
+
+  #startReapLoop() {
+    const interval = setInterval(this.#reap, this.#interval)
+    this.#reapIntervalId = interval
+  }
+
+  stopReapLoop() {
+    clearInterval(this.#reapIntervalId)
+    this.#reapIntervalId = undefined
+  }
+}
+
+
+
